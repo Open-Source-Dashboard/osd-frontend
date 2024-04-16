e8cfa8a32914e245ff284a4d960cfc0c9511953a
lana-z <lana@levelupeconomy.com>
2024-04-16 15:39:05 -0700

    added about us page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,9 @@ const Footer = () => {
   return (
     <footer className="p-4 text-center text-white bg-purple-500">
       <div className="flex flex-col items-center justify-center space-y-4">
+        <a href="https://github.com/orgs/Open-Source-Dashboard/repositories" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
+          Link to our Repo
+        </a>
         <Link to="/about-us" className="text-white hover:text-gray-300">
           About Us
         </Link>
--- src/views/AboutUs.js
+++ src/views/AboutUs.js
@@ -1,1 +1,43 @@
+import React from 'react';
+
+
+const teamMembers = [
+  { pfp: '', name: 'Tammy', role: 'project role', linkedin: 'https://www.linkedin.com/in/tammytdo/', github: 'url', donut: 'Glazed'},
+  { pfp: '', name: 'Lana', role: 'project role', linkedin: 'https://www.linkedin.com/in/lanazumbrunn/', github: 'url', donut: 'Cream Bismarck'},
+  { pfp: '', name: 'Andrea', role: 'project role', linkedin: 'https://www.linkedin.com/in/and-riley/', github: 'url', donut: 'Jelly Filled'},
+];
+
+const AboutUs = () => {
+  return (
+    <div className="min-h-screen p-8 bg-gray-100">
+      <div className="container mx-auto text-center">
+        <h1 className="mb-8 text-4xl font-bold text-gray-800">Our Team</h1>
+        <div className="mb-8 text-lg">
+          <p>Interested in our work? Visit our GitHub repository to learn more and contribute!</p>
+          <a href="//https://github.com/orgs/Open-Source-Dashboard/repositories" target="_blank" rel="noopener noreferrer" className="inline-block p-4 mt-4">
+            View our GitHub Repo
+          </a>
+        </div>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+          {teamMembers.map((member, index) => (
+            <div key={index} className="p-6 bg-white rounded-lg shadow-lg">
+                <div className="mb-4">
+                    {/* Display GitHub Profile Pic */}
+                    <img src={member.pfp} alt={`${member.name}'s GitHub Profile Pic`} className="object-cover w-40 h-40 mx-auto bg-gray-200 rounded-full"/>
+                </div>
+                <h3 className="text-xl font-semibold">{member.name}</h3>
+                <p className="text-gray-600">Project Role: {member.role}</p>
+                <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700">LinkedIn</a>
+                <br/>
+                <a href={member.github} target="_blank" rel="noopener noreferrer" >GitHub Profile</a>
+                <p className="text-gray-600">Favorite Donut: {member.donut}</p>
+          </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
 
