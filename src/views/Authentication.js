4d302bb408b9daa66b17d3799471faa538c52e3d
Caleb Hemphill <calebhemp@gmail.com>
2024-04-25 00:58:24 -0400

    Add authentication view

---
+++ src/views/Authentication.js
@@ -0,0 +1,12 @@
+import Login from '../auth/Login';
+
+const Authentication = () => {
+  return (
+    <div>
+      <h1>Authentication</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default Authentication;
