deffd67718b67b8f1bf0e5bb8c063a2c4ef523d5
Caleb Hemphill <calebhemp@gmail.com>
2024-04-25 00:58:09 -0400

    Add temp logged in display

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
@@ -6,26 +6,59 @@ const Login = () => {
   const location = useLocation();
   const navigate = useNavigate();
 
+  const authAPI = process.env.AUTH_API_URL;
+
+  const [userToken, setUserToken] = useState({});
+
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const code = searchParams.get('code');
 
-    if (code) {
-      // Send the code to the backend to exchange it for an access token
-      axios
-        .post('/api/auth/github/callback/', { code })
-        .then((response) => {
-          // Save the token in local storage
-          localStorage.setItem('github_token', response.data.token);
-          navigate('/'); // Redirect to a different page
-        })
-        .catch((error) => {
-          console.error('Error exchanging code for token:', error);
-        });
-    }
-  }, [location, navigate]);
+    // if (code) {
+    //   // Send the code to the backend to exchange it for an access token
+    //   axios
+    //     .post('/api/auth/github/callback/', { code })
+    //     .then((response) => {
+    //       // Save the token in local storage
+    //       localStorage.setItem('github_token', response.data.token);
+    //       navigate('/'); // Redirect to a different page
+    //     })
+    //     .catch((error) => {
+    //       console.error('Error exchanging code for token:', error);
+    //     });
+    // }
 
-  return <div>Loading...</div>;
+    if (code) {
+      const userToken = getGithubToken(code);
+      setUserToken(userToken);
+    }
+
+  }, [location]);
+
+  async function getGithubToken(userCode) {
+    try {
+      const userTokenResponse = await axios.post(authAPI, { code: userCode });
+      const userToken = userTokenResponse.data.token;
+      return userToken;
+    } catch (error) {
+      console.error('Error exchanging code for token:', error);
+    }
+  }
+
+  return (
+    <>
+      {userToken.token ? (
+        <div>
+          <h1>Logged in!</h1>
+          <p>{JSON.stringify(userToken.token)}</p>
+        </div>
+      ) : (
+        <div>
+          <h1>Not logged in!</h1>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Login;
