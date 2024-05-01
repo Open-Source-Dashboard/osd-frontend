4b262952d207bca0b11dc8b66a1bc0d2fc65fdcc
Caleb Hemphill <calebhemp@gmail.com>
2024-05-01 11:01:07 -0400

    Add AuthProvider to App

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './global.css';
 
+import { AuthProvider } from './auth/AuthContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
