26ca7290c72a994038fe6906861b7be996a2d101
Caleb Hemphill <calebhemp@gmail.com>
2024-05-01 11:02:13 -0400

    Refactor AuthButtons component to use AuthContext

--- src/auth/AuthButtons.jsx
+++ src/auth/AuthButtons.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
+import { AuthButtonUrl } from './AuthContext';
 import { FaGithub } from 'react-icons/fa';
 
 const AuthButtons = () => {
-  const client_id = process.env.REACT_APP_GITHUB_CLIENT_ID;
-  // const redirectUri=process.env.REACT_APP_GITHUB_CALLBACK_URL;
-
-  // const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirectUri}`;
-  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}`;
-
   return (
     <div>
       <a
-        href={githubAuthUrl}
+        href={AuthButtonUrl()}
         className='flex items-center px-3 py-2 font-medium text-black rounded-md textbase hover:text-bg-purple-600'
       >
         <FaGithub className='mr-2' />
