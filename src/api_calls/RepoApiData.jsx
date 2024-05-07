6d071d90ef0db560812406063d28c9cab5d58a5b
lana-z <lana@levelupeconomy.com>
2024-05-06 18:12:35 -0700

    add api call structure

---
+++ src/api_calls/RepoApiData.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const RepoApiData = () => {
+  const [repoData, setRepoData] = useState({});
+  const API_SERVER_URL = process.env.API_SERVER_URL;
+
+  const getRepoData = async () => {
+    try {
+      const response = await axios.get(`${API_SERVER_URL}/osd/repos/`);
+      console.log('response:', response);
+      const repoData = response.data;
+      setRepoData(repoData);
+    } catch (error) {
+      console.error('Error getting repo data:', error);
+    }
+  };
+
+  return (
+    <>RepoApiData</>
+    // <>
+    //   <div>
+    //     {repoData.length > 0 ? (
+    //       <ul>
+    //         {repoData.map((repo) => (
+    //           <li key={repo.id}>{repo.name}</li>
+    //         ))}
+    //       </ul>
+    //     ) : (
+    //       <p>No repo data</p>
+    //     )}
+    //   </div>
+    // </>
+  );
+}
+
+export default RepoApiData;
