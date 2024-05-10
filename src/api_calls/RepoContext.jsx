8e91afedd6d07c35467224f610eaf5331e49227d
tammytdo11 <tammytdo11@gmail.com>
2024-05-10 00:52:55 -0700

    populate dashboard with opensource projects

--- src/api_calls/RepoContext.jsx
+++ src/api_calls/RepoContext.jsx
@@ -1,20 +1,36 @@
-import React, { createContext, useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import sampleRepoResponse from "../sampleData/sampleRepoResponse.json"; // keep for testing
 export const RepoContext = createContext();
 
 export const RepoProvider = ({ children }) => {
-  const [repoData, setRepoData] = useState([]);
+    const [featuredRepoResult, setFeaturedRepoResult] = useState([]);
+    const [latestContributorsResult, setLatestContributorsResult] = useState([]);
+    const [popularReposResult, setPopularReposResult] = useState([]);
+    const [repositoryData, setRepositoryData] = useState([]);
+  
   const API_SERVER_URL = process.env.REACT_APP_API_SERVER_URL;
 
   useEffect(() => {
     const getRepoData = async () => {
       try {
         const response = await axios.get(`${API_SERVER_URL}/repos`);
-        const { repositories } = response.data;
-        setRepoData(repositories); 
+        const {
+          featured_repo_result,
+          latest_contributors_result,
+          popular_repos_result,
+          repositories,
+        } = response.data;
+
+        setFeaturedRepoResult(featured_repo_result);
+        setLatestContributorsResult(latest_contributors_result);
+        setPopularReposResult(popular_repos_result);
+        setRepositoryData(repositories);
+
+        // use for testing to minimize api calls and increase rendering speed.
+        // setRepositoryData(sampleRepoResponse)
       } catch (error) {
-        console.error('Error getting repo data:', error);
+        console.error("Error getting repo data:", error);
       }
     };
 
@@ -22,10 +38,8 @@ export const RepoProvider = ({ children }) => {
   }, [API_SERVER_URL]);
 
   return (
-    <RepoContext.Provider value={{ repoData }}>
-      {children}
-    </RepoContext.Provider>
+    <RepoContext.Provider value={{ repositoryData }}>{children}</RepoContext.Provider>
   );
 };
 
-export default RepoContext
+export default RepoContext;
--- src/components/PeruseProjects.jsx
+++ src/components/PeruseProjects.jsx
@@ -2,27 +2,40 @@ import React, { useContext } from "react";
 import { RepoContext } from "../api_calls/RepoContext";
 
 const PeruseProjects = () => {
-  const { repoData } = useContext(RepoContext);
-  
-    return (
-      <div className="p-6 bg-white rounded-lg shadow-md">
-        <h2 className="mb-4 text-xl font-bold">Peruse Projects</h2>
-        <p>Here Are Some Projects You Might Like</p>
+  const { repositoryData } = useContext(RepoContext);
+  const getThreeRepos = repositoryData.slice(0, 3);
 
-          {repoData.length > 0 ? (
-            <ul>
-              {repoData.map((repo) => (
-                <li key={repo.id} className="text-sm">
-                  <a href={repo.url} target="_blank" rel="noreferrer">{repo.name}</a>
-                </li>
-              ))}
-            </ul>
-        ) : (
-          <p>Error</p>
-        )}
-      </div>
-    );
-  };
-  
-  export default PeruseProjects;
-  
+
+  return (
+    <div className="p-6 bg-white rounded-lg shadow-md w-2/3">
+      <h2 className="mb-4 text-xl font-bold">Peruse Projects</h2>
+
+      {repositoryData.length > 0 ? (
+         <ul className="grid grid-cols-1 gap-4">
+         {getThreeRepos.map((repo) => (
+            <li key={repo.id} className="border border-gray-300 bg-gray-100 p-4 rounded-lg shadow-md">
+            <h2>
+               <a href={repo.url} target="_blank" rel="noreferrer">
+                 {repo.name}
+               </a>
+             </h2>
+             <p>Description: {repo.description}</p>
+             <p>⭐ {repo.stargazers_count}</p>
+             <p>Topics: {repo.topics.map((topic, index) => (
+               <span key={topic}>
+                 {topic}
+                 {index !== repo.topics.length - 1 && ', '}
+               </span>
+             ))}
+             </p>
+           </li>
+         ))}
+       </ul>
+      ) : (
+        <p>Error</p>
+      )}
+    </div>
+  );
+};
+
+export default PeruseProjects;
