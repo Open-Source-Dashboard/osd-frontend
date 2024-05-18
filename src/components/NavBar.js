3412af52fea76e32e54b562dce4b407954d93ecd
tammytdo11 <tammytdo11@gmail.com>
2024-05-18 02:12:40 -0400

    resize and stampcard background

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,11 @@ const NavBar = () => {
   
   return (
     <nav className="p-0.5 bg-gradient-to-r from-primary to-secondary">
-      <div className="flex items-center justify-between w-full p-1 mx-auto bg-white bg-opacity-50 border-gray-300 rounded-lg">
+      <div className="flex items-center justify-between w-full pl-1 pr-1 mx-auto bg-gray-900 border-gray-300 rounded-lg">
         <Link to="/" className="flex items-center space-x-3">
-        <h1 className="self-center p-3 pb-2 mt-2 xs:text-2xl sm:text-3xl md:text-4xl lg:text-4xl">            Donut Dashboard
-
-        <h2 className="self-center py-0 text-lg font-normal sm:text-lg md:text-lg lg:text-xl mt-0 mb-0">              Virtually Sweet Rewards for Opensource Project Contributions
-            </h2>
+          <h1 className="self-center p-3 pb-2 mt-2 xs:text-m sm:text-l md:text-xl lg:text-2xl">Donut Dashboard
+          <h2 className="self-center py-0 text-lg font-normal sm:text-m md:text-l lg:text-l mt-0 mb-0">Virtually Sweet Rewards for Opensource Project Contributions</h2>
           </h1>
-          
         </Link>
         <div className="flex items-center space-x-4">
           {/* <Link to="/getting-started" className="px-3 py-2 text-base font-medium rounded-md">
--- src/components/StampCard.jsx
+++ src/components/StampCard.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useState, useEffect } from 'react';
+import { lazy, Suspense, useState, useEffect } from "react";
 
 const StampCard = () => {
   const [userDonutsCount, setUserDonutsCount] = useState(1); // We give everyone a donut to start!
@@ -27,15 +27,15 @@ const StampCard = () => {
   }, [stampedDonuts]);
 
   function getUserDonutsCount() {
-    return 1; // TODO: fetch user donuts count from API 
+    return 1; // TODO: fetch user donuts count from API
   }
 
   function getUserDonutBoxCount() {
-    return Math.floor(userDonutsCount / 12); 
+    return Math.floor(userDonutsCount / 12);
   }
-  
+
   function randomizeStampedDonutsIcons(numberOfStamps) {
-    const stampsArray = new Array(numberOfStamps).fill('');
+    const stampsArray = new Array(numberOfStamps).fill("");
     stampsArray.forEach((_, index) => {
       stampsArray[index] = Math.floor(Math.random() * 6) + 1; // Increase from 6 if add more donut designs
     });
@@ -43,7 +43,7 @@ const StampCard = () => {
   }
 
   function randomizeEmptyDonutsIcons(numberOfEmptyDonuts) {
-    const emptyDonutsArray = new Array(numberOfEmptyDonuts).fill('');
+    const emptyDonutsArray = new Array(numberOfEmptyDonuts).fill("");
     emptyDonutsArray.forEach((_, index) => {
       emptyDonutsArray[index] = Math.floor(Math.random() * 6) + 1;
     });
@@ -51,29 +51,48 @@ const StampCard = () => {
   }
 
   return (
-    <div className='flex-grow h-auto row-1-card'>
-      <div className='w-full p-4 bg-orange-300 rounded-sm shadow-inner'>
-        <h2 className='mb-2 text-xl font-bold text-center text-gray-800'>
-          My Donut Rewards</h2>
-        <div className='grid grid-cols-3 grid-rows-4 gap-2 mx-auto mb-5 place-items-center'>
-          {stampedDonuts.map((iconNumber, index) => {
-            return (
-              <div key={index} className='flex items-center justify-center w-16 h-16 m-1 bg-orange-100 rounded-lg shadow-md'>
-                <div className='flex items-center justify-center w-16 h-16 p-1'> 
-                <ColorDonutIcon number={iconNumber} />
-              </div>
-              </div>
-            );
-          })}
-          {emptyDonuts.map((iconNumber, index) => {
-            return (
-              <div key={index} className='flex items-center justify-center w-16 h-16 p-1 bg-orange-100 rounded-lg shadow-md'>
-                <OutlineDonutIcon number={iconNumber} />
-              </div>
-            );
-          })}
+    <div className="flex-grow h-auto w-3/4 row-1-card">
+      <h2 className="mb-2 text-xl font-bold mt-0">My Contributions</h2>
+
+      <div className="flex items-center justify-center w-full">
+        <div
+          className="w-5/6 p-4 rounded-md shadow-inner"
+          style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL}/sprinkle_background.jpg)`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <div className="grid grid-cols-3 grid-rows-4 gap-2 mx-auto mb-3 place-items-center">
+            {stampedDonuts.map((iconNumber, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex items-center justify-center w-16 h-16 m-1 bg-orange-100 rounded-lg shadow-md"
+                >
+                  <div className="flex items-center justify-center w-16 h-16 p-1">
+                    <ColorDonutIcon number={iconNumber} />
+                  </div>
+                </div>
+              );
+            })}
+            {emptyDonuts.map((iconNumber, index) => {
+              return (
+                <div
+                  key={index}
+                  className="flex items-center justify-center w-16 h-16 p-1 bg-orange-100 rounded-lg shadow-md"
+                >
+                  <OutlineDonutIcon number={iconNumber} />
+                </div>
+              );
+            })}
+          </div>
+          <div className="bg-white p-3 rounded-md shadow-md">
+            <p className="text-xs italic text-center text-gray-800">
+              Earn a donut for every opensource project commit.
+            </p>
+          </div>
         </div>
-        <p className='mt-3 text-xs italic text-center text-gray-800 '>Earn a donut for every opensource project commit. </p>
       </div>
     </div>
   );
@@ -82,7 +101,7 @@ const StampCard = () => {
 const OutlineDonutIcon = ({ number }) => {
   const componentName = `Outline${number}`;
   const Component = lazy(() =>
-    import('./DonutIcons').then((module) => ({
+    import("./DonutIcons").then((module) => ({
       default: module[componentName],
     }))
   );
@@ -98,7 +117,7 @@ const OutlineDonutIcon = ({ number }) => {
 const ColorDonutIcon = ({ number }) => {
   const componentName = `Color${number}`;
   const Component = lazy(() =>
-    import('./DonutIcons').then((module) => ({
+    import("./DonutIcons").then((module) => ({
       default: module[componentName],
     }))
   );
