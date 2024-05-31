fe5f82b8131b2e1d00191bee8fac90bc3cb490c7
tammytdo11 <tammytdo11@gmail.com>
2024-05-31 00:18:35 -0700

    add page not found component

--- src/components/NavLeft.jsx
+++ src/components/NavLeft.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
 import AuthButtons from '../auth/AuthButtons';
+import { useAuth } from "../auth/AuthContext";
 import { FaGithub } from 'react-icons/fa';
 
 const NavLeft = () => {
   const isAuthenticated = !!localStorage.getItem('github_token');
-  
+  const { user } = useAuth();
+  let username = user.github_username || 'null'
+
   return (
     <div>
       <div className="flex flex-col p-3 text-sm rounded-lg justify-left">
@@ -24,7 +27,7 @@ const NavLeft = () => {
           )}
         <div className="flex flex-col space-y-10 text-sm justify-left">
         <a
-            href="https://github.com/tammytdo#js-contribution-activity"
+            href={`https://github.com/${username}#js-contribution-activity`}
             target="_blank"
             rel="noopener noreferrer"
           >
