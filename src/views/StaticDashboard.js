f446b790ddb5e1aa7657c24d5989fe002ba4b414
tammytdo11 <tammytdo11@gmail.com>
2024-05-31 01:18:49 -0700

    add NavLeftStatic

--- src/views/StaticDashboard.js
+++ src/views/StaticDashboard.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import StampCardStatic from '../components/StampCardStatic';
-import MyCommitsStatic from '../components/MyCommitsStatic';
-import LatestContributors from '../components/LatestContributors';
-import PeruseProjects from '../components/PeruseProjects';
-import FeaturedProject from '../components/FeaturedProject';
-import CommitGraphStatic from '../components/CommitGraphStatic';
-import NavLeft from '../components/NavLeft';
+import NavLeftStatic from '../components/NavLeftStatic';
 
+// Reduced the static dashboard for testing purposes
 const StaticDashboard = () => {
   return (
     <div className='flex flex-col min-h-screen'>
@@ -16,7 +11,7 @@ const StaticDashboard = () => {
           <h1 style={{fontSize:'2em', color:'yellow', marginLeft:'2%'}}>** Login to see the live dashboard **</h1>
         {/* Row 1 */}
         <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-3'>
-          <NavLeft />
+          <NavLeftStatic />
         </div>
       </main>
     </div>
