4b4a6767dab042362d94288f0f5295b0d77a0acd
tammytdo11 <tammytdo11@gmail.com>
2024-05-31 01:26:42 -0700

    remove comment

--- src/views/StaticDashboard.js
+++ src/views/StaticDashboard.js
@@ -9,7 +9,6 @@ const StaticDashboard = () => {
       </div>
       <main className='flex-grow'>
           <h1 style={{fontSize:'2em', color:'yellow', marginLeft:'2%'}}>** Login to see the live dashboard **</h1>
-        {/* Row 1 */}
         <div className='grid grid-cols-1 gap-4 p-4 md:grid-cols-3'>
           <NavLeftStatic />
         </div>
