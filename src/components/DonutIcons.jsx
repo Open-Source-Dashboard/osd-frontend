1fc4a7b4286bdc41a08dd7e036a9b4cbf25f3979
tammytdo11 <tammytdo11@gmail.com>
2024-05-31 16:26:55 -0700

    reduce to 1 greyed out donut

--- src/components/DonutIcons.jsx
+++ src/components/DonutIcons.jsx
@@ -1,10 +1,6 @@
 // greyed out donut icons
 import outline1 from '../assets/donut-icons/outline/1.png';
-import outline2 from '../assets/donut-icons/outline/2.png';
-import outline3 from '../assets/donut-icons/outline/3.png';
-import outline4 from '../assets/donut-icons/outline/4.png';
-import outline5 from '../assets/donut-icons/outline/5.png';
-import outline6 from '../assets/donut-icons/outline/6.png';
+
 // color donut icons
 import color1 from '../assets/donut-icons/color/1.png';
 import color2 from '../assets/donut-icons/color/2.png';
@@ -18,19 +14,19 @@ export const Outline1 = () => {
   return <img src={outline1} alt='outlined donut' />;
 };
 export const Outline2 = () => {
-  return <img src={outline2} alt='outlined donut' />;
+  return <img src={outline1} alt='outlined donut' />;
 };
 export const Outline3 = () => {
-  return <img src={outline3} alt='outlined donut' />;
+  return <img src={outline1} alt='outlined donut' />;
 };
 export const Outline4 = () => {
-  return <img src={outline4} alt='outlined donut' />;
+  return <img src={outline1} alt='outlined donut' />;
 };
 export const Outline5 = () => {
-  return <img src={outline5} alt='outlined donut' />;
+  return <img src={outline1} alt='outlined donut' />;
 };
 export const Outline6 = () => {
-  return <img src={outline6} alt='outlined donut' />;
+  return <img src={outline1} alt='outlined donut' />;
 };
 
 // color donut components
