df0bcd8d4912b17a8900bf229d25ce1a55b3d26a
tammytdo11 <tammytdo11@gmail.com>
2024-06-10 02:03:37 -0700

    refactor auth files

--- src/auth/LoginButton.jsx
+++ src/auth/LoginButton.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { FaGithub } from 'react-icons/fa';
+import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const ghClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
 
 const LoginButton = () => {
+  const githubOAuthUrl = `https://github.com/login/oauth/authorize?client_id=${ghClientId}`;
 
   return (
     <a
-      href={`https://github.com/login/oauth/authorize?client_id=${ghClientId}`}
-      className='flex items-center pt-2 pb-6 text-xl rounded-md font-small textbase hover:text-light-blue'>
+      href={githubOAuthUrl}
+      className="flex items-center pt-2 pb-6 text-xl rounded-md font-small textbase hover:text-light-blue"
+    >
       <span>Login</span>
-      <FaGithub className='ml-2' />
+      <FaGithub className="ml-2" />
     </a>
   );
 };
