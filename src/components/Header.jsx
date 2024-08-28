eae1bc7be31d1a05a1fef879cb06fe916691e7b7
lana-z <lana@levelupeconomy.com>
2024-08-27 19:44:23 -0700

    link to osd org consistently across components, link to dashboard view in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
-const SubHeader = () => {
+import { Link } from 'react-router-dom';
+
+const Header = () => {
   return (
     <div className="flex items-center w-full h-full">
-      <div to="/" className="flex items-center bg-gray rounded-md w-full m-0">
+      <Link to="/" className="flex items-center w-full m-0 rounded-md bg-gray">
         <h1 className="mt-2 ml-4 text-3xl font-bold text-pink">Donut Dashboard</h1>
         <div className="pb-2 mx-6 sub-header">
           <h2 className=''>Virtually Sweet Rewards for Opensource Project Contributions</h2>
         </div>
-      </div>
     </div>
   );
 };
 
-export default SubHeader;
+export default Header;
