b8246fa3adbe7782c7792a7fb599cc25a1d0894f
lana-z <lana@levelupeconomy.com>
2024-09-10 22:07:52 -0700

    play with sm and md breakpoints, round MapOfUsers component rendering on DashboardStatic

--- src/views/DashboardStatic.js
+++ src/views/DashboardStatic.js
@@ -18,15 +18,15 @@ const DashboardStatic = () => {
       className="flex-col m-3 space-y-5 md:h-screen md:gap-4 md:grid md:grid-cols-18 md:grid-rows-12">
         <div className="flex items-center md:grid md:row-start-1 md:row-span-2 md:col-span-9">
         <Header />
-        <div className="pl-3 md:hidden"> 
+        <div className="pl-3 sm:hidden"> 
           <AuthButtons />
         </div>
         </div>
-        <div className="hidden md:grid md:col-span-7 md:row-start-1 md:row-span-5 min-w-50">
+        <div className="hidden md:grid md:col-span-7 md:row-start-1 md:row-span-5">
           <LatestContributors/>
         </div>
         <NavLeftStatic />
-        <div className="flex items-center justify-center md:grid md:col-start-2 md:col-span-4 min-w-40">
+        <div className="flex items-center justify-center sm:grid md:col-start-2 sm:col-span-4 ">
           <StampCardStatic />
         </div>
         <MyCommitsStatic />
@@ -34,7 +34,7 @@ const DashboardStatic = () => {
         <div className="flex p-2 rounded-lg md:row-span-5 md:col-span-7 bg-gray md:col-start-10">
           <FeaturedProject />
         </div>
-        <div className="flex hidden p-2 md:grid min-h-60 md:col-span-5 bg-gray">
+        <div className="flex hidden p-2 rounded-lg md:grid min-h-60 md:col-span-5 bg-gray">
           <MapOfUsers />
         </div>
         <div className="flex p-2 rounded-lg md:col-span-11 md:row-span-10 bg-gray md:col-start-6 md:row-start-11">
@@ -44,7 +44,7 @@ const DashboardStatic = () => {
         <div className="md:hidden">
           <LatestContributors/>
         </div>
-        <div className="flex p-2 md:hidden min-h-60 bg-gray" >
+        <div className="flex p-2 rounded-lg md:hidden min-h-60 bg-gray" >
           <MapOfUsers />
         </div>
         <div className="md:hidden">
