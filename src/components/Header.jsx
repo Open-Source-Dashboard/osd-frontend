bdd0d491190627076e4612df8ba3509ff55d77b2
lana-z <lana@levelupeconomy.com>
2024-10-08 11:08:34 -0700

    revert Header layout on lg screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div >
-      <Link to="/" className="flex flex-col items-center w-full rounded-md bg-gray">
-        <h1 className="flex mt-2 ml-4 text-3xl font-bold text-pink">Donut Dashboard</h1>
-        <div className="sub-header">
+      <Link to="/" className="flex flex-col lg:flex-row items-center w-full rounded-md bg-gray">
+        <h1 className="mt-3 lg:mt-0 lg:ml-4 text-3xl font-bold text-pink">Donut Dashboard</h1>
+        <div className="sub-header lg:pl-6">
           <h2 className="text-sm md:text-xl"> Virtually Sweet Rewards for Opensource Project Contributions</h2>
         </div>
       </Link>
